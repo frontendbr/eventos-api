cca6084be5d07c6d23d0bcdc73e5cbe878704a54
Rogerio Angeliski <angeliski@hotmail.com>
2017-07-10 08:37:06 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,27 +1,27 @@
 'use strict';
 
-module.exports.listEvent = function
+  module.exports.listEvent = function () {
 
-  var examples = [];
-  examples.push({
-  "date" : "20/02/2017",
-  "price" : "100.00",
-  "id" : 1,
-  "shortDescription" : "Evento para frontends discutirem sobre assuntos bacanas",
-  "title" : "Front In Sampa",
-  "local" : "Avenida Paulista, 1000",
-  "eventLink" : "http://link-para-o-evento.com.br"
-});
-
-  examples.push({
-  "date" : "20/02/2017",
-  "price" : "100.00",
-  "id" : 2,
-  "shortDescription" : "Evento para frontends discutirem sobre assuntos bacanas",
-  "title" : "Front In Sampa",
-  "local" : "Avenida Paulista, 1000",
-  "eventLink" : "http://link-para-o-evento.com.br"
+    var examples = [];
+    examples.push({
+    "date" : "20/02/2017",
+    "price" : "100.00",
+    "id" : 1,
+    "shortDescription" : "Evento para frontends discutirem sobre assuntos bacanas",
+    "title" : "Front In Sampa",
+    "local" : "Avenida Paulista, 1000",
+    "eventLink" : "http://link-para-o-evento.com.br"
   });
 
-  return examples;
-}
+    examples.push({
+    "date" : "20/02/2017",
+    "price" : "100.00",
+    "id" : 2,
+    "shortDescription" : "Evento para frontends discutirem sobre assuntos bacanas",
+    "title" : "Front In Sampa",
+    "local" : "Avenida Paulista, 1000",
+    "eventLink" : "http://link-para-o-evento.com.br"
+    });
+
+    return examples;
+  }
