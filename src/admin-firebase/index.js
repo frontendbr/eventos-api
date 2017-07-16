e8c0f432e9e57b72dd9f29c9f5a2b219b598c0de
Rogerio Angeliski <angeliski@hotmail.com>
2017-07-16 00:13:05 -0300

    Ajuste na inicialização do firebase-admin

--- src/admin-firebase/index.js
+++ src/admin-firebase/index.js
@@ -2,12 +2,15 @@ var admin = require("firebase-admin");
 
 module.exports = () => {
 
-  var serviceAccount = require("./frontend-br-eventos-firebase-adminsdk-cwau8-feba252aa4.json");
+  const serviceAccount = require("./frontend-br-eventos-firebase-adminsdk-cwau8-feba252aa4.json");
+  if (serviceAccount.type) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://frontend-br-eventos.firebaseio.com"
+    });
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://frontend-br-eventos.firebaseio.com"
-  });
+    return admin;
+  }
 
-  return admin;
+  return {};
 }
