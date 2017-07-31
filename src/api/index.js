aea941b85b5fca77251a93b4d1cd7b9b356ade1b
Rogerio Angeliski <rogerio.angeliski@bluesoft.com.br>
2017-07-30 21:18:04 -0300

    Ajustando testes

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,12 @@
 const { version }  = require('../../package.json');
 const { Router } = require('express');
 const Docs = require('./docs');
-const Login = require('./login');
 
 module.exports = ({ config, db }) => {
   console.info('Init API module');
 	let api = Router();
 
   api.use(Docs({config, db}));
-  api.use(Login({config, db}));
 
 	api.get('/', (req, res, next) => {
 		res.json({ version });
