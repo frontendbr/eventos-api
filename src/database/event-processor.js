516164810e33fa7b191ff8ddd1ffa4276a13fa74
Rogerio Angeliski <angeliski@hotmail.com>
2017-08-16 22:56:48 -0300

    Adicionando inicio do filtro por keyword

--- src/database/event-processor.js
+++ src/database/event-processor.js
@@ -1,3 +1,9 @@
+
+const eventMatchFilter = ({ event, filter }) => {
+    //TODO insensitive case
+    return (event.title.indexOf(filter.keywords) > -1);
+}
+
 module.exports = {
     process: ({ filter, snapshot }) => {
         const { month, state, keywords, offset = 0, limit = 30 } = filter;
@@ -6,12 +12,15 @@ module.exports = {
 
         const events = [];
         snapshot.forEach((childSnapshot) => {
-
             if (events.length < limit) {
                 if (offsetCount >= offset) {
                     const event = childSnapshot.val();
                     event.key = childSnapshot.key;
-                    events.push(event);
+                    if (eventMatchFilter({ event, filter })) {
+                        events.push(event);
+                    } else {
+                        offsetCount--;
+                    }
                 }
             } else {
                 return true;
