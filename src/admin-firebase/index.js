4fe74133d72ca7200804039f2b895d796270ebb6
Rogerio Angeliski <angeliski@hotmail.com>
2017-10-09 21:35:27 -0300

    Adicionando ajuste na inicialização do admin-firebase

--- src/admin-firebase/index.js
+++ src/admin-firebase/index.js
@@ -3,22 +3,29 @@ var admin = require("firebase-admin");
 module.exports = ({ config }) => {
   const serviceAccount = require(config.serviceAccountAdminPath);
   const databaseURL = "https://frontend-br-eventos.firebaseio.com";
+
+  const  { FIREBASE_ADMIN_LOAD_TYPE, 
+           FIREBASE_ADMIN_PROJECT_ID, 
+           FIREBASE_ADMIN_CLIENT_EMAIL, 
+           FIREBASE_ADMIN_PRIVATE_KEY } = process.env;
   
-  if (serviceAccount.type) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL
-    });
-
-
+  if(FIREBASE_ADMIN_LOAD_TYPE === 'SECRET') {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: "<PROJECT_ID>",
-        clientEmail: "foo@<PROJECT_ID>.iam.gserviceaccount.com",
-        privateKey: "-----BEGIN PRIVATE KEY-----\n<KEY>\n-----END PRIVATE KEY-----\n"
+        projectId: FIREBASE_ADMIN_PROJECT_ID,
+        clientEmail: FIREBASE_ADMIN_CLIENT_EMAIL,
+        privateKey: FIREBASE_ADMIN_PRIVATE_KEY
       }),
       databaseURL
-    });
+    });    
+  } else {
+    if (serviceAccount.type) {
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL
+      });
+  
+  }
 
     return admin;
   }
