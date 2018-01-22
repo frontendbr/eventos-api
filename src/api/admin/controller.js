e319b9d9629bd29f1f5032e76ad4edb36a9dd27c
Rogerio Angeliski <angeliski@hotmail.com>
2018-01-22 01:07:04 -0200

    Adicionando testes da parte de admini

--- src/api/admin/controller.js
+++ src/api/admin/controller.js
@@ -1,8 +1,13 @@
 import Administrator from './model'
 import { success } from './../../constants'
 
-export const create = (req, res, next) =>
+export const create = (req, res) =>
   Administrator.create(new Administrator(req.body))
     .then(admin => admin)
     .then(success(res))
-    .catch(next)
+    .catch((err) => {
+      return res.status(500).json({
+        status: false,
+        data: err
+      })
+    })
